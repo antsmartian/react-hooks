992154bebb6c2b9698fef39a032675abdc47dd42
Anto Aravinth <anto.aravinth.cse@gmail.com>
2019-01-31 21:12:37 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-function App() {
+function useRandomNames(initialValue) {
+    const [inputValue, setInputValue] = useState(initialValue);
+    const [isLoading, setIsLoading] = useState(false);
+    const [names, setNames] = useState([]);
 
-    let [names, setNames] = useState([]);
+    const handleInputChange = e => {
+        setInputValue(e.target.value);
+    };
 
     useEffect(() => {
-        fetch("https://uinames.com/api/?amount=10&region=Romania")
+        setIsLoading(true);
+        fetch(`https://uinames.com/api/?amount=10&region=${inputValue}`)
             .then(response => response.json())
             .then(data => {
-                setNames(data)
+                setNames(data);
+                setIsLoading(false);
             })
-    }, []);
+    }, [inputValue]);
+
+    return {
+        inputValue,
+        onChange : handleInputChange,
+        names,
+        isLoading
+    }
+}
+
+function App() {
+
+    let randomNames = useRandomNames('Romania');
 
     return (
         <div>
-            <div>
-                {
-                    names.map((items, i) => (
-                        <div key={i}>
-                            {items.name} {items.gender}
-                        </div>
-                    ))
-                }
-            </div>
+            <p>
+                Enter the region : {' '}
+                <input value={randomNames.inputValue}
+                       onChange={randomNames.onChange} />
+            </p>
+
+            {randomNames.isLoading ? <p>Loading....</p> :
+                <div>
+                    {
+                        randomNames.names.length >=0 &&
+                            randomNames.names.map((items, i) => (
+                                <div key={i}>
+                                    {items.name} {items.gender}
+                                </div>
+                            ))
+                    }
+                </div>
+            }
         </div>
     )
 
